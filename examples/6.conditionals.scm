d4ffb25f622851ea0509b114306ddd57a412aaee
ansuz <ansuz@transitiontech.ca>
2015-02-11 22:36:12 -0500

    cheating for my github streak again today. just playin the game

---
+++ examples/6.conditionals.scm
@@ -0,0 +1,14 @@
+; 6.conditionals.scm
+
+#|
+  You'll have a hard time writing any significant piece of software
+  without using conditionals, so we'd better cover how to do that.
+|#
+
+; here's the most basic type of conditional statement
+(display (if #t "hello!" "Goodbye!"))
+(newline)
+
+; here's the most basic type of conditional statement
+(display (if #f "hello!" "Goodbye!"))
+(newline)
