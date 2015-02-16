b859bd958aa4ab76a88b1ad1cafa085b65163aa7
ansuz <ansuz@transitiontech.ca>
2015-02-15 22:35:07 -0500

    examples/6.conditionals.scm : multi-expression conditionals.

--- examples/6.conditionals.scm
+++ examples/6.conditionals.scm
@@ -12,3 +12,15 @@
 ; here's the most basic type of conditional statement
 (display (if #f "hello!" "Goodbye!"))
 (newline)
+
+; use cond for conditionals with more than two conditions
+; suppose we want to check the type of a value
+(define (typeof x)
+  (cond ; cond takes a variable number of arguments
+    ((string? x) "string")
+    ((list? x) "list")
+    ((vector? x) "vector")
+    (else "idk man")))
+
+(display (typeof '(0 1 2 3)))
+(newline)
