fa08360b7c2ebb8f35d8fad4decd4d6c5d01a79f
ansuz <ansuz@transitiontech.ca>
2016-03-06 11:25:12 +0100

    add a second form

--- apis/vars.scm
+++ apis/vars.scm
@@ -6,3 +6,11 @@
 
 (f '(pew pew pew) 1 2 3)
 ; You passed 4 arguments to this function
+
+; here's a shorter way to do the same thing
+(define (g . vars)
+    (format #t "You passed ~A arguments to this function\n"
+        (length vars)))
+
+(g 1 2 3)
+; You passed 3 arguments to this function
